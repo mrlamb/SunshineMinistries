212f85b3ff16f593f749f57024477d6b016906cc
Matthew Lamb <lamb.matthew@gmail.com>
2019-08-28 16:55:18 -0500

    Initial

---
+++ SunshineMinistriesConsole/SunshineMinistriesConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunshineMinistriesConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new sunshinedataEntities())
+            {
+                var clients = from b in context.clients
+                            select b;
+                var client = context.clients.FirstOrDefault();
+                Console.Write(client.firstname);
+            }
+            
+            
+        }
+    }
+}
